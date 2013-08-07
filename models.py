7101604d6ade6331ce5bfc7400f97f00db570625
Ian Kelly <ltsquigs@gmail.com>
2013-08-06 23:33:21 -0700

    model changes

--- models.py
+++ models.py
@@ -7,12 +7,21 @@ class Board(models.Model):
 	name = models.CharField(max_length=200)
 	restricted = models.BooleanField(default=False)
 	allowed_groups = models.ManyToManyField(Group)
+	# views?
+	# posts?
 
 class Topic(models.Model):
 	board = models.ForeignKey(Board)
 	title = models.CharField(max_length=200, default='')
 	deleted = models.BooleanField(default=False)
+	anchored = models.BooleanField(default=False)
+	anchoredToFeed = models.BooleanField(default=False)
+	locked = models.BooleanField(default=False)
 
+	# views ?
+	posts = models.IntegerField(default=0)
+
+	#firstMessage/lastMessage?
 
 class Message(models.Model):
 	topic = models.ForeignKey(Topic)
