57cfa7476b2a655e2def06e1392544185c7b2faa
Ian Kelly <ltsquigs@gmail.com>
2013-08-06 23:54:00 -0700

    no prefix

--- views.py
+++ views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from forums.models import ForumTopic
+from forums.models import Topic
 
 def index(request):
 
-    latest_topics_list = ForumTopic.objects.order_by('-date_updated')[:20]
+    latest_topics_list = Topic.objects.order_by('-date_updated')[:20]
     template = loader.get_template('forums/index.html')
 
     context = RequestContext(request, {
