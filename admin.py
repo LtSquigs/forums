19f8de3e2df1ee2443286a96fbcb0af831571c53
Ian Kelly <ltsquigs@gmail.com>
2013-08-06 23:24:29 -0700

    typo

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from forms.models import Board
-from forms.models import Topic
-from forms.models import Message
+from forums.models import Board
+from forums.models import Topic
+from forums.models import Message
 
 admin.site.register(Board)
 admin.site.register(Topic)
