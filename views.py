81df3fa9f61e70dfad49fb5d17f343815b005c24
Ian Kelly <ian.kelly@cbsinteractive.com>
2013-08-07 17:03:24 -0700

    boar dlist

--- views.py
+++ views.py
@@ -2,14 +2,18 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 from forums.models import Topic
+from forums.models import Board
 
 def index(request):
 
     latest_topics_list = Topic.objects.order_by('-date_updated')[:20]
+    board_list = Board.objects.order_by('id')
+
     template = loader.get_template('forums/index.html')
 
     context = RequestContext(request, {
         'latest_topics_list' : latest_topics_list
+        'board_list' : board_list
     })
     
     return HttpResponse(template.render(context))
